966517cab7a6692ce8cc21df781dbb730e901906
Panos Sakkos <panos.sakkos@gmail.com>
2025-04-16 19:33:54 +0300

    update { minimal-code } keymap

--- keyboards/lily58/keymaps/minimal-code/keymap.c
+++ keyboards/lily58/keymaps/minimal-code/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // clang-format off
     [_BASE] = LAYOUT(
-                    KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,              KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
+                    KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,               KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
                     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,               KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
                     KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G,               KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
                  KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC,     KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_DEL,
@@ -51,13 +51,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------.                    ,-----------------------------------------.
      * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-     * | Tab  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
+     * | Tab  |      |      |      |      |      |                    | Prev | VolDn| VolUp| Next |      |      |
      * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
      * | Esc  |      |      |      |      |      |-------.    ,-------| Left | Down |  Up  | Right|      |      |
      * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
-     * |LShift|      |      |      |      |      |-------|    |-------| Prev | VolDn| VolUp| Next |      |Delete|
+     * |LShift|      |      |      |      |      |-------|    |-------|      | BriDn| BriUp|      |      |Delete|
      * `-----------------------------------------/       /     \      \-----------------------------------------'
-     *                   | LCtl | LAlt | LGUI | / Play  /       \ Mute \  | BriDn| BriUp|TG(LO)|
+     *                   | LCtl | LAlt | LGUI | / Play  /       \ Mute \  |      |      |TG(LO)|
      *                   |      |      |      |/       /         \      \ |      |      |      |
      *                   `----------------------------'           '------''--------------------'
      */
@@ -65,10 +65,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
     [_LOWER] = LAYOUT(
                     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,           KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-                    KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5,               KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
+                    KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,          KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO, KC_NO,
                     KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,          KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
-                KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO, KC_DEL,
-                    KC_LCTL, KC_LALT, KC_LGUI, KC_MPLY,                 KC_MUTE, KC_BRMD, KC_BRMU, TG(_LOWER))};
+                KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_BRMD, KC_BRMU, KC_NO, KC_NO, KC_DEL,
+                    KC_LCTL, KC_LALT, KC_LGUI, KC_MPLY,                 KC_MUTE, KC_NO, KC_NO, TG(_LOWER))};
 
 // clang-format on
 
